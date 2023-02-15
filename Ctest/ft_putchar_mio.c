c4595ba2eb5ee3681f1697648fe47224504d8ab6
elijeldres <elijeldres@gmail.com>
2023-02-15 23:04:36 +0100

    primeros ejercicios en c

---
+++ Ctest/ft_putchar_mio.c
@@ -0,0 +1,15 @@
+// C program to demonstrate putchar() method
+
+#include <stdio.h>
+
+int main()
+{
+
+	// Get the character to be written
+	char ch = 'c';
+
+	// Write the Character to stdout
+	putchar(ch);
+
+	return (0);
+}
