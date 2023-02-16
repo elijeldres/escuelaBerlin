a8a4d0a0a86bbef55e9c2deb7b2c819b5150a64c
elijeldres <elijeldres@gmail.com>
2023-02-16 18:52:02 +0100

    operaciones matematicas basicas

---
+++ Ctest/basic_div.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	// int x; Declaration but we can put it just in the line below 
+	int x = 10;  //Initialization 
+
+	
+	int y = x/2 ; // Expression is anything that can be evaluated to one value 
+
+	printf("El resultado es: %i \n cuando el valor de x es %i", y, x );
+
+		//es necesario " %i"para decir que sera un digito (integer) 
+       		//	\n para que luego empiece en nueva linea
+       		//, y   los argumentos estan separados por coma. este caso pasamos 3 argumentos la declaracion El resultado es: y el valor de Y coma el valor de x  o el numero que queramos imprimir	
+	
+	return (0);
+
+}
+
+
+// c es a space insensitive language. 
+// syntax = form format of the language, ; = ({ etc
